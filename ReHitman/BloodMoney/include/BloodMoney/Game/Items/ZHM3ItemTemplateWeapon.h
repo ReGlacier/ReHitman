f0dddcd0aaaf8ab130442f15e283d72b89ffd218
DronCode <alexandrleutin@gmail.com>
2020-12-10 01:29:38 +0300

    Reversed EHM3ItemType (all types), ZHM3ItemTemplateWeapon, ZHM3ItemWeapon

---
+++ ReHitman/BloodMoney/include/BloodMoney/Game/Items/ZHM3ItemTemplateWeapon.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <Glacier/ZItemTemplateWeapon.h>
+#include <BloodMoney/Game/Items/EHM3ItemType.h>
+
+namespace Hitman::BloodMoney
+{
+    class ZHM3ItemTemplateWeapon : Glacier::ZItemTemplateWeapon
+    {
+    public:
+        //vftable
+        virtual EHM3ItemType GetHM3ItemType();
+        virtual float GetRecoilRandom();
+        virtual float GetRecoilStrengthX();
+        virtual float GetRecoilStrengthY();
+        virtual void* GetLaserIndicator();
+        virtual void* GetRedDot();
+        virtual bool CanPackIntoSuitcase();
+        virtual void* GetScope();
+        virtual int GetSilencerType();
+        virtual float GetWeaponStrength();
+        virtual int Get1stPersonAimId();
+        virtual int Get1stPersonRecoilId();
+        virtual const char* GetAnimNameActorReload(int);
+        virtual const char* GetAnimNameActorChamber();
+        virtual void* GetClipParticleControl(bool);
+
+        //data (total size is 0x1A0, ZItemTemplateWeapon size is 0x15C)
+        int m_field15C;
+        int m_field160;
+        int m_field164;
+        int m_field168;
+        int m_field16C;
+        int m_field170;
+        int m_field174;
+        int m_field178;
+        int m_field17C;
+        int m_field180;
+        int m_field184;
+        int m_field188;
+        int m_field18C;
+        int m_field190;
+        int m_field194;
+        int m_field198;
+        int m_field19C;
+    };
+}
---
+++ ReHitman/BloodMoney/include/BloodMoney/Game/Items/ZHM3ItemWeapon.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include <Glacier/GlacierFWD.h>
+#include <Glacier/ZItemWeapon.h>
+#include <BloodMoney/Game/Items/EHM3ItemType.h>
+
+namespace Hitman::BloodMoney
+{
+    class ZHM3ItemWeapon : public Glacier::ZItemWeapon
+    {
+    public:
+        //vftable
+        virtual EHM3ItemType GetHM3ItemType();
+        virtual void ChamberActivateAnimation();
+        virtual void UseItemActivateAnimation();
+        virtual const char* GetAnimNameActorReload();
+        virtual const char* GetAnimNameActorChamber();
+        virtual int GetClipBoneNr();
+        virtual void InitializeMetaKeyEvents(Glacier::Animation::Header* );
+        virtual void UpdateMetaKeyEvents();
+        virtual void UpdateReloadShellAnim();
+        virtual void UpdateReloadShellAnimMetaKeys();
+        virtual void FireAnimCallback(Glacier::Animation::ActiveAnimation*, float, float, unsigned int);
+        virtual bool IsDetectable(); //always true
+
+        //data (total size is 0x15C, ZItemWeapon size is 0xDC)
+        int m_fieldDC;
+        int m_fieldE0;
+        int m_fieldE4;
+        int m_fieldE8;
+        int m_fieldEC;
+        int m_fieldF0;
+        int m_fieldF4;
+        int m_fieldF8;
+        int m_fieldFC;
+        int m_field100;
+        int m_field104;
+        int m_field108;
+        int m_clipBoneNr;
+        int m_field110;
+        int m_field114;
+        int m_field118;
+        int m_field11C;
+        int m_field120;
+        Glacier::ZLNKOBJ* m_field124;
+        int m_field128;
+        int m_field12C;
+        int m_field130;
+        int m_field134;
+        int m_field138;
+        int m_field13C;
+        int m_field140;
+        int m_field144;
+        int m_field148;
+        int m_field14C;
+        int m_field150;
+        int m_field154;
+        int m_field158;
+    };
+
+}
